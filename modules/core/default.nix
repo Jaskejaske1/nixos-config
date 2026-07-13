e786a731468bf0c2f6c1589346d140790ff78fd4
Jasper Savels <jasper.savels@gmail.com>
2026-07-14 01:57:19 +0200

    chore: quiet resume console output

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -104,6 +104,12 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams = [
+    "quiet"
+    "udev.log_level=3"
+    "systemd.show_status=auto"
+    "rd.udev.log_level=3"
+  ];
 
   networking.networkmanager.enable = true;
 
